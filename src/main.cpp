4661d5e06954cffdc5626b34d64d7169a0c09d76
Evgeny Popodyanets <DarthKurt@users.noreply.github.com>
2025-05-23 20:55:28 +0200

    readability

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include <thread>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
     float aspectRatio = (float)width / (float)height;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -12,52 +13,77 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glMatrixMode(GL_MODELVIEW);
 }
 
-int main() {
-    if (!glfwInit()) {
+GLFWwindow *initUI()
+{
+    if (!glfwInit())
+    {
         std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
+        return nullptr;
     }
 
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Ant Colony Simulation", NULL, NULL);
-    if (!window) {
+    GLFWwindow *window = glfwCreateWindow(600, 600, "Ant Colony Simulation", NULL, NULL);
+    if (!window)
+    {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return -1;
+        return nullptr;
     }
 
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    return window;
+}
 
+void render(GLFWwindow *window, const Simulation &simulation)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    const auto &colony = simulation.getColony();
+    colony.render(window);
+
+    const auto &antManager = simulation.getAntManager();
+    antManager.render(window);
+
+    const auto &foodManager = simulation.getFoodManager();
+    foodManager.render(window);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
+void controlFrameRate(const std::__1::chrono::steady_clock::time_point &startTime, const float targetFrameTimeMs)
+{
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto frameDuration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    if (frameDuration.count() < targetFrameTimeMs)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(targetFrameTimeMs - frameDuration.count())));
+    }
+}
+
+int main()
+{
+    auto window = initUI();
+
+    if (!window)
+        return -1;
+
+    // Seed randomizer
     srand(static_cast<unsigned>(time(nullptr)));
 
     Simulation simulation;
 
-    const float targetFrameTimeMs = 1000.0f / 30.0f; // 30 FPS
+    // Set framerate
+    const float targetFrameTimeMs = 1000.0f / 30.0f;
 
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window))
+    {
         auto startTime = std::chrono::high_resolution_clock::now();
 
         simulation.update();
 
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        const Colony &colony = simulation.getColony();
-        colony.render(window);
-
-        const AntManager &antManager = simulation.getAntManager();
-        antManager.render(window);
-
-        const FoodManager&foodManager = simulation.getFoodManager();
-        foodManager.render(window);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-        auto endTime = std::chrono::high_resolution_clock::now();
-        auto frameDuration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
-        if (frameDuration.count() < targetFrameTimeMs) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(targetFrameTimeMs - frameDuration.count())));
-        }
+        render(window, simulation);
+        controlFrameRate(startTime, targetFrameTimeMs);
     }
 
     glfwDestroyWindow(window);
